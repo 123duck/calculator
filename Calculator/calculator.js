2c1816888a41d145312613d9eb10c00b90e8d14e
Jayanti Ghising <89849337+123duck@users.noreply.github.com>
2022-09-05 20:15:55 +0545

    Add files via upload

---
+++ Calculator/calculator.js
@@ -0,0 +1,16 @@
+function clearscreen()
+{
+    document.getElementById("output").value="";
+}
+
+function display(value)
+{
+    document.getElementById("output").value+=value;
+}
+
+function calculate()
+{
+    var x = document.getElementById("output").value;
+    var y = eval(x);
+    document.getElementById("output").value= y;
+}
